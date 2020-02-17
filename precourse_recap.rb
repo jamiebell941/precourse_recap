ec623a3d60dd3349e1813e90b8ef2ecf3e5b01c6
jamiebell941 <jamiebell941@gmail.com>
2020-02-17 16:16:44 +0000

    added examples of ruby

---
+++ precourse_recap.rb
@@ -0,0 +1,22 @@
+x = 123
+y = 100
+if x > y
+  p "#{x} is higher than #{y}"
+else
+  p "#{x} is lower than #{y}"
+end
+
+def greeting
+  p "hello world"
+end
+
+rappers = ["Kendrick Lamar", "Childish Gambino", "Mac Miller", "Vince Staples"]
+
+in_bruges = {"Director" => "Martin McDonagh", "Lead" => "Colin Farrel", "Support" => "Brendon Gleeson"}
+
+counter = 1
+target = 13
+while counter <= target
+  p "currently counter is at #{counter}"
+  counter = counter + 1
+end
